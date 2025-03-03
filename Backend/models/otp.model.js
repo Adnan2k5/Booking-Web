94341dd515f7a0843711260274e296967ba9fcff
Md Saquib Jawed <saquibjawed4444@gmail.com>
2025-03-03 19:40:05 +0530

    Add OTP model for user verification with expiration

---
+++ Backend/models/otp.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const otpSchema = new mongoose.Schema(
+    {
+        loginId: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true,
+        },
+        otp: {
+            type: Number,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            expires: 300 // 300 seconds = 5 minutes
+        }
+    }
+);
+
+export const Otp = mongoose.model("Otp", otpSchema);
