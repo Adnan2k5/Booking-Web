f2355fb2791f7c1ae523a338a83399df51fb4b7d
Md Saquib Jawed <saquibjawed4444@gmail.com>
2025-04-27 00:27:02 +0530

    Add messaging functionality with routes, controller, and model

--- Backend/server.js
+++ Backend/server.js
@@ -12,6 +12,7 @@ import tickerRoute from "./routes/ticket.route.js"
 import userRoute from "./routes/user.routes.js"
 import termRouter from "./routes/terms.routes.js";
 import documentRouter from "./routes/document.routes.js";
+import messageRoute from "./routes/message.routes.js";
 import { initCloudinary } from "./utils/cloudinary.js";
 
 
@@ -28,6 +29,7 @@ app.use("/api/user", userRoute);
 app.use("/api/ticket", tickerRoute);
 app.use("/api/terms", termRouter);
 app.use("/api/document", documentRouter);
+app.use("/api/messages", messageRoute);
 
 
 const PORT = process.env.PORT || 8080;
