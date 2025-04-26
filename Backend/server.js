4a35eb34187eb4e87aa95ddb85a719f91757ae50
Md Saquib Jawed <saquibjawed4444@gmail.com>
2025-04-27 00:22:24 +0530

    Add terms management functionality with controller, model, and routes

--- Backend/server.js
+++ Backend/server.js
@@ -10,6 +10,7 @@ import authRoute from "./routes/auth.routes.js"
 import adventureRoute from "./routes/adventure.routes.js"
 import tickerRoute from "./routes/ticket.route.js"
 import userRoute from "./routes/user.routes.js"
+import termRouter from "./routes/terms.routes.js";
 import { initCloudinary } from "./utils/cloudinary.js";
 
 
@@ -24,6 +25,7 @@ app.use("/api/auth", authRoute);
 app.use("/api/adventure", adventureRoute);
 app.use("/api/user", userRoute);
 app.use("/api/ticket", tickerRoute);
+app.use("/api/terms", termRouter);
 
 
 const PORT = process.env.PORT || 8080;
