ec3549e2b0705cacb77fe506da5b30ac51fab38d
Md Saquib Jawed <saquibjawed4444@gmail.com>
2025-05-02 00:54:57 +0530

    Remove PostAdventure component and its route from the application

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -24,7 +24,6 @@ import Dash_Hotels from "./Pages/Admin/SubPages/Hotels"
 import Dash_Tickets from "./Pages/Admin/SubPages/Tickets"
 import Dash_Terms from "./Pages/Admin/SubPages/Terms"
 import Dash_Declation from "./Pages/Admin/SubPages/Declaration"
-import PostAdventure from "./Pages/PostAdventure"
 
 // Lazy loaded components
 const LoginPage = lazy(() => import("./Pages/LoginPage"))
@@ -138,7 +137,6 @@ const App = () => {
                 <Route path="/admin/tickets" element={<Dash_Tickets />} />
                 <Route path="/admin/terms" element={<Dash_Terms />} />
                 <Route path="/admin/declaration" element={<Dash_Declation />} />
-                <Route path="/admin/post-adventure" element={<PostAdventure />} />
               </Route>
             </Routes>
           </Suspense>
