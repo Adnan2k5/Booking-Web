0e128d0f6e747d66a139cac7b2c7341f6fd6a601
Saquib Jawed <138011435+saquibjawedbit@users.noreply.github.com>
2025-06-02 01:46:38 +0530

    we2

---
+++ Backend/routes/itemBooking.routes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { createBooking } from "../controllers/itemBooking.controller.js";
+import { verifyJWT } from '../middlewares/auth.middleware.js';
+
+const router = express.Router();
+
+// Middleware to verify JWT token
+router.use(verifyJWT);
+
+router.get(
+    '/create', createBooking
+);
