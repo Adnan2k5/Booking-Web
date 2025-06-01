f664d073cc034dcc6893d710195008e0c665683b
Saquib Jawed <138011435+saquibjawedbit@users.noreply.github.com>
2025-06-02 01:49:26 +0530

    changed routes

--- Client/src/Api/booking.api.js
+++ Client/src/Api/booking.api.js
@@ -1,7 +1,7 @@
 import { axiosClient } from '../AxiosClient/axios';
 
 export const createBooking = async (name) => {
-  const res = await axiosClient.get(`/api/booking/create?name=${name}`, {
+  const res = await axiosClient.get(`/api/itemBooking/create?name=${name}`, {
     withCredentials: true,
   });
   return res;
