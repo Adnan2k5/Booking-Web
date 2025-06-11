4f720b4a6edd26aaf575917a001c4e3a5f4e9212
Md Saquib Jawed <saquibjawed4444@gmail.com>
2025-06-12 01:08:06 +0530

    feat: add translation support for cart items and integrate language detection middleware

--- Backend/routes/cart.routes.js
+++ Backend/routes/cart.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { getCartItems, addToCart, updateCartItem, removeCartItem, clearCart } from "../controllers/cart.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { languageMiddleware } from "../middlewares/language.middleware.js";
 
 const router = express.Router();
 
-// All routes require authentication
+// All routes require authentication and language detection
 router.use(verifyJWT);
+router.use(languageMiddleware);
 
 // Get current user's cart
 router.get("/", getCartItems);
